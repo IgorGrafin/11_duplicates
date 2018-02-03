a92e817e669ec43902064fbe25cebe88b9efc3da
Igor Grafin <apaticmail@gmail.com>
2018-02-03 23:05:54 +0300

    initial

--- duplicates.py
+++ duplicates.py
@@ -1,43 +1,38 @@
 import os
+import sys
 
 
 def make_files_dict(path):
+    files_dict = {}
     tree = os.walk(path)
-    for cur_folder, subfolders, files in tree:
-        for file in files:
-            full_path = os.path.join(cur_folder, file)
-            files_dict[full_path] = [file, os.path.getsize(full_path)]
-
+    for cur_folder, subfolders, files_names in tree:
+        for file_name in files_names:
+            full_path = os.path.join(cur_folder, file_name)
+            file_size = os.path.getsize(full_path)
+            if (file_name, file_size) in files_dict.keys():
+                files_dict[file_name, file_size].append(full_path)
+            else:
+                files_dict[file_name, file_size] = [full_path]
     return files_dict
 
 
-def find_duplicates(files_dict):
-    duplicates_dict = {}
-    for files in files_dict.keys():
-        for key in files_dict.keys():
-            if files != key\
-                    and files_dict[files][0] == files_dict[key][0]\
-                    and files_dict[files][1] == files_dict[key][1]:
-                print(files)
-                if not duplicates_dict.get(files_dict[key][0]):
-                    duplicates_dict[files_dict[key][0]] = [key]
-                else:
-                    duplicates_dict[files_dict[key][0]].append(key)
-
-    return duplicates_dict
+def beauty_print(files_dict):
+    for file_name, file_size in files_dict:
+        if len(files_dict[file_name, file_size]) > 1:
+            print("Found {0} files {1} with size {2} :".format
+                (
+                len(files_dict[file_name, file_size]),
+                file_name,
+                file_size
+            ))
+            print("\n".join(files_dict[file_name, file_size]))
 
 
 if __name__ == '__main__':
-    files_dict = {}
-
-    path = "E:\\GitHub\\devman\\11_duplicates\\foldertest"
-
+    try:
+        path = sys.argv[1]
+    except IndexError:
+        sys.exit("Please, put a file name as a parameter.\n"
+                 "For example: 'python duplicates.py E:\GitHub' ")
     files_dict = make_files_dict(path)
-    # for x in files_dict.keys():
-    #     print(x, files_dict[x])
-    duplicates = find_duplicates(files_dict)
-
-    for file_name in duplicates.keys():
-        print("Одинаковые файлы ", file_name, "найдены по следующим адресам:")
-        for file_path in duplicates[file_name]:
-            print(file_path)
+    beauty_print(files_dict)
